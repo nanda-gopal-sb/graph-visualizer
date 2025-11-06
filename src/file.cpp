f70823460f7103d20ccbf95c88688ce714d98ac0
nanda-gopal-sb <nandagopal.nsb@gmail.com>
2025-11-06 20:59:33 +0530

    refactor file retrieval to use std::filesystem for improved readability and performance

--- src/file.cpp
+++ src/file.cpp
@@ -2,30 +2,39 @@
 #include <dirent.h>
 #include <vector>
 #include <string>
-int main(int argc, char *argv[])
-{
-    std::vector<std::string> file_names;
-    printf("%s\n", argv[1]);
-    dirent *dir_pointer;
-    DIR *dir = opendir(argv[1]);
-    if (dir != NULL)
-    {
-        char *filename = new char[258];
+#include <fstream>
+#include <filesystem>
+std::vector<std::string> file_names;
+using namespace std::filesystem;
 
-        while ((dir_pointer = readdir(dir)) != NULL)
+void getFiles(std::string path)
+{
+    if (exists(path) && is_directory(path))
+    {
+        for (const auto &entry :
+             directory_iterator(path))
         {
-            if (dir_pointer->d_name[0] != '.')
+            if (entry.path().filename().generic_string()[0] != '.')
             {
-                sprintf(filename, "%s/%s", argv[1], dir_pointer->d_name);
-                std::string cpp_str(filename);
-                file_names.push_back(cpp_str);
+                // std::cout << "File: " << entry.path() << std::endl;
+                if (is_directory(entry.path()))
+                {
+                    getFiles(entry.path());
+                }
+                else
+                {
+                    file_names.push_back(entry.path());
+                }
             }
-            // printf("%s/%s\n", argv[1], dir_pointer->d_name);
-        }
-        for (std::string x : file_names)
-        {
-            std::cout << x << "\n";
         }
     }
-    closedir(dir);
+}
+
+int main(int argc, char *argv[])
+{
+    getFiles(argv[1]);
+    for (const std::string &filename : file_names)
+    {
+        std::cout << filename << std::endl;
+    }
 }
